9470d926a6265b108331414e93c0a9ba0341e3b4
Stepichano <100946597+Stepichano@users.noreply.github.com>
2022-03-11 02:27:38 +0700

    фиксация main

--- Config.py
+++ Config.py
@@ -1,1 +1,6 @@
-sfsdfsdfsdfsdsdfsd adad djaslkhjadfs
+import pygame
+sc = pygame.display.set_mode((600, 600))
+sc.fill((255, 255, 255))
+
+image1 = pygame.image.load("C:\\Users\\admin\\Documents\\GitHub\\Simple-Fight\\Pictures\\голова.png")
+
--- Main.py
+++ Main.py
@@ -1,11 +1,17 @@
 import pygame
-import Config
+import PIL
+from PIL import Image
+from Config import* 
+
 
 class Head:
-    __init__(self, ):
-
-
-
-        эээ ну типа
-
+    def __init__(self, pic_head, x=300, y=255):
+        self.pic_head = pic_head
+        self.x = x
+        self.y = y
     
+    def draw_items_head(self):
+        sc.blit(self.pic_head, (300, 350))        
+
+s = Head(image1, 35, 21)
+s.draw_items_head()
